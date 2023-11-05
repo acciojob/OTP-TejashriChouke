7636bc1bb3d4ffaea967829f4673031c85a5d1d5
TejashriChouke <139746868+TejashriChouke@users.noreply.github.com>
2023-11-05 23:51:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function verifyOTP() {
+    var phoneNumber = document.getElementById("phone").value;
+    var otp = document.getElementById("otp").value;
+
+    // Here, you can add logic to verify the OTP with the backend.
+    // For example, you can make an API call to the server to verify the OTP.
+
+    // Dummy logic for demonstration purposes
+    if (otp === "123456") {
+        document.getElementById("message").textContent = "OTP Verified successfully!";
+    } else {
+        document.getElementById("message").textContent = "Invalid OTP. Please try again.";
+    }
+}
+
